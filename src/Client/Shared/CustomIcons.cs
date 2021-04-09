a7504879153b686be2b923975aac685ad1d47969
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2021-04-08 22:21:56 -0700

    Rewrote standings view. Standings are now scoped to a phase and grouped by encounter. Standings can be ungrouped to show the entire phase. Closes #6.

--- src/Client/Shared/CustomIcons.cs
+++ src/Client/Shared/CustomIcons.cs
@@ -11,6 +11,15 @@ namespace ValhallaLootList.Client.Shared
         // https://materialdesignicons.com/icon/hospital
         public static string Cross { get; } = "<path d=\"M18 14H14V18H10V14H6V10H10V6H14V10H18\" />";
 
+        // https://materialdesignicons.com/icon/code-brackets
+        public static string Brackets { get; } = "<path d=\"M15,4V6H18V18H15V20H20V4M4,4V20H9V18H6V6H9V4H4Z\" />";
+
+        // https://materialdesignicons.com/icon/set-merge
+        public static string SetMerge { get; } = "<path d=\"M2 7V9H7V7H2M12 9V11H9V13H12V15L15 12L12 9M17 9V15H22V9H17M2 11V13H7V11H2M2 15V17H7V15H2Z\" />";
+
+        // https://materialdesignicons.com/icon/set-split
+        public static string SetSplit { get; } = "<path d=\"M17 7V9H22V7H17M2 9V15H7V9H2M12 9V11H9V13H12V15L15 12L12 9M17 11V13H22V11H17M17 15V17H22V15H17Z\" />";
+
         // https://materialdesignicons.com/icon/eye-off
         public static string EyeOff { get; } = "<path d=\"M11.83,9L15,12.16C15,12.11 15,12.05 15,12A3,3 0 0,0 12,9C11.94,9 11.89,9 11.83,9M7.53,9.8L9.08,11.35C9.03,11.56 9,11.77 9,12A3,3 0 0,0 12,15C12.22,15 12.44,14.97 12.65,14.92L14.2,16.47C13.53,16.8 12.79,17 12,17A5,5 0 0,1 7,12C7,11.21 7.2,10.47 7.53,9.8M2,4.27L4.28,6.55L4.73,7C3.08,8.3 1.78,10 1,12C2.73,16.39 7,19.5 12,19.5C13.55,19.5 15.03,19.2 16.38,18.66L16.81,19.08L19.73,22L21,20.73L3.27,3M12,7A5,5 0 0,1 17,12C17,12.64 16.87,13.26 16.64,13.82L19.57,16.75C21.07,15.5 22.27,13.86 23,12C21.27,7.61 17,4.5 12,4.5C10.6,4.5 9.26,4.75 8,5.2L10.17,7.35C10.74,7.13 11.35,7 12,7Z\" />";
     }

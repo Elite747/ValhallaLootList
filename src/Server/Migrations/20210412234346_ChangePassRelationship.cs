e98cd40995a89907265e906a5373bb90db0256cb
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2021-04-14 03:30:28 -0700

    Added missing file header.

--- src/Server/Migrations/20210412234346_ChangePassRelationship.cs
+++ src/Server/Migrations/20210412234346_ChangePassRelationship.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿// Copyright (C) 2021 Donovan Sullivan
+// GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ValhallaLootList.Server.Migrations
 {

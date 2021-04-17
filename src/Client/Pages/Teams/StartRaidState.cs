3436ad59bf4303face09c2ac6335a1ab3a1ad082
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2021-04-16 23:33:54 -0700

    Switched to wizard-style raid creation.

---
+++ src/Client/Pages/Teams/StartRaidState.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (C) 2021 Donovan Sullivan
+// GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+using System.Collections.Generic;
+using ValhallaLootList.DataTransfer;
+
+namespace ValhallaLootList.Client.Pages.Teams
+{
+    public class StartRaidState
+    {
+        public byte Phase { get; set; }
+
+        public Dictionary<long, CharacterDto> Attendees { get; } = new();
+    }
+}

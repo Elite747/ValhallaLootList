960e4a969c0adb9d1ac002ee1d3d408ce0b2b719
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2021-04-28 19:26:38 -0700

    Removed unused function.

--- src/Client/wwwroot/app.js
+++ src/Client/wwwroot/app.js
@@ -13,15 +13,5 @@
             element.classList.add('mud-dialog-scrollable');
             element.classList.add(`mud-dialog-scrollable-${mode}`);
         }
-    },
-    updateUserRoles: async function (prop, claims) {
-        let user = await AuthenticationService.instance._userManager.getUser();
-        if (claims) {
-            user.profile[prop] = claims;
-        }
-        else {
-            delete user.profile[prop];
-        }
-        await AuthenticationService.instance._userManager.storeUser(user);
     }
 };

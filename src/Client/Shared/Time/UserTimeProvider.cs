f8be4a8ad1052699a4c130ff67caf2565e3b1a7a
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2021-04-28 19:12:21 -0700

    Improvements to raid calendar. Calendar now shows future scheduled raids. Raid phases are now depicted with an icon. Calendar supports state management using urls.

--- src/Client/Shared/Time/UserTimeProvider.cs
+++ src/Client/Shared/Time/UserTimeProvider.cs
@@ -4,17 +4,20 @@
 using System;
 using System.Threading.Tasks;
 using ValhallaLootList.Client.Data;
+using ValhallaLootList.Helpers;
 
 namespace ValhallaLootList.Client.Shared
 {
     public class UserTimeProvider
     {
         private readonly LocalStorageService _localStorage;
+        private readonly TimeZoneInfo _serverTimeZone;
         private const string _storageKey = "timemode";
 
         public UserTimeProvider(LocalStorageService localStorage)
         {
             _localStorage = localStorage;
+            _serverTimeZone = TimeZoneInfo.FromSerializedString("Eastern Standard Time;-300;(UTC-05:00) Eastern Time (US & Canada);Eastern Standard Time;Eastern Daylight Time;[01:01:0001;12:31:2006;60;[0;02:00:00;4;1;0;];[0;02:00:00;10;5;0;];][01:01:2007;12:31:9999;60;[0;02:00:00;3;2;0;];[0;02:00:00;11;1;0;];];");
             _ = InitializeAsync();
         }
 
@@ -29,11 +32,27 @@ namespace ValhallaLootList.Client.Shared
             ModeChanged?.Invoke();
         }
 
+        public DateTimeOffset Now => Mode switch
+        {
+            TimeFormatterMode.Original => DateTimeOffset.UtcNow.ToTimeZone(_serverTimeZone),
+            TimeFormatterMode.Local => DateTimeOffset.Now,
+            TimeFormatterMode.Utc => DateTimeOffset.UtcNow,
+            _ => DateTimeOffset.Now
+        };
+
+        public DateTime Today => Now.Date;
+
+        public DateTimeOffset FromServerTimeOfDay(DateTime date, TimeSpan timeOfDay)
+        {
+            var dateTime = date.Date.Add(timeOfDay);
+            return Convert(new DateTimeOffset(dateTime, _serverTimeZone.GetUtcOffset(dateTime)));
+        }
+
         public DateTimeOffset Convert(DateTimeOffset date)
         {
             return Mode switch
             {
-                TimeFormatterMode.Original => date,
+                TimeFormatterMode.Original => date.ToTimeZone(_serverTimeZone),
                 TimeFormatterMode.Local => date.ToLocalTime(),
                 TimeFormatterMode.Utc => date.ToUniversalTime(),
                 _ => date

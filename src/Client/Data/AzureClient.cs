eb615635c1e02012352f8b2ace6777af135a6ff8
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2021-06-22 15:04:38 -0700

    Switched to XDocument for xml deserialization. XmlSerializer was not compatible with blazor's tree shaking.

--- src/Client/Data/AzureClient.cs
+++ src/Client/Data/AzureClient.cs
@@ -2,13 +2,11 @@
 // GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 using System;
-using System.Diagnostics;
-using System.IO;
+using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
+using System.Xml.Linq;
 using ValhallaLootList.Client.Data.Containers;
 
 namespace ValhallaLootList.Client.Data
@@ -28,32 +26,37 @@ namespace ValhallaLootList.Client.Data
 
         public string GetDomain() => _domain;
 
-        public async Task<object?> GetContainerAsync(string container, CancellationToken cancellationToken = default)
+        public async Task<AzureContainerResponse> GetContainerAsync(string container, CancellationToken cancellationToken = default)
         {
-            using var response = await _httpClient.GetAsync($"https://{_domain}/{container}?restype=container&comp=list", cancellationToken);
+            await using var stream = await _httpClient.GetStreamAsync($"https://{_domain}/{container}?restype=container&comp=list", cancellationToken);
 
-            if (response.IsSuccessStatusCode)
+            var document = await XDocument.LoadAsync(stream, LoadOptions.None, cancellationToken);
+
+            if (document.Root is null)
             {
-                var str = await response.Content.ReadAsStringAsync(cancellationToken);
-                var buffer = Encoding.UTF8.GetBytes(str);
-                using var stream = new MemoryStream(buffer);
-                var serializer = new XmlSerializer(typeof(AzureContainerResponse));
-                var azureContainerResponse = serializer.Deserialize(stream) as AzureContainerResponse;
-
-                Debug.Assert(azureContainerResponse is not null);
-                Debug.Assert(azureContainerResponse.Blobs is not null);
-                foreach(Blob blob in azureContainerResponse.Blobs)
-                {
-                    Debug.Assert(blob.name is not null);
-                    Debug.Assert(blob.url is not null);
-                }
-
-                azureContainerResponse.ContainerName = container;
-
-                return azureContainerResponse;
+                throw new Exception("Invalid Xml Response");
             }
 
-            return await response.Content.ReadAsStringAsync(cancellationToken);
+            var containerName = document.Root.Attribute("ContainerName")?.Value ?? container;
+            var blobs = new List<Blob>();
+
+            var blobElements = document.Root.Element("Blobs")?.Elements();
+
+            if (blobElements is not null)
+            {
+                foreach (var element in blobElements)
+                {
+                    var name = element.Element("Name")?.Value;
+                    var url = element.Element("Url")?.Value;
+
+                    if (name?.Length > 0 && url?.Length > 0)
+                    {
+                        blobs.Add(new(name, url));
+                    }
+                }
+            }
+
+            return new(containerName, blobs);
         }
 
         private void Dispose(bool disposing)

c57ad388aabc32072252a0e6054edd87dbb3a542
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2021-07-26 12:52:06 -0700

    Added missing file header.

--- src/Server/Migrations/20210726184633_AddPhase2.cs
+++ src/Server/Migrations/20210726184633_AddPhase2.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (C) 2021 Donovan Sullivan
+// GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ValhallaLootList.Server.Migrations

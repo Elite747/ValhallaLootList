add0097f8a40242478cb55102362c0ca0338dc2b
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2022-02-09 14:41:59 -0800

    Added new raid audit page. Closes #105

---
+++ src/Shared/DataTransfer/AuditDropDto.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (C) 2021 Donovan Sullivan
+// GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+namespace ValhallaLootList.DataTransfer;
+
+public class AuditDropDto
+{
+    public uint ItemId { get; init; }
+
+    public string ItemName { get; init; } = string.Empty;
+
+    public long RaidId { get; init; }
+
+    public DateTimeOffset RaidDate { get; init; }
+
+    public long DropId { get; init; }
+
+    public string TeamName { get; init; } = string.Empty;
+}

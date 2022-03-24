233471f2ba6504c49e959e597493e344cde7c695
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2022-03-24 14:55:17 -0700

    Added discord invite permalink.

---
+++ src/Server/Controllers/DiscordController.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (C) 2021 Donovan Sullivan
+// GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ValhallaLootList.Server.Controllers;
+
+[Route("[controller]"), AllowAnonymous]
+public class DiscordController : ControllerBase
+{
+    public IActionResult Get() => Redirect("https://discord.com/invite/DTrFTaG");
+}

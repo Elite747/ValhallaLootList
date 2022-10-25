d806542458b4f4177b1315a3f42ee424176752d0
Donovan Sullivan <48695653+Elite747@users.noreply.github.com>
2022-10-25 01:14:00 -0700

    Fixed wowhead interop issue from missing method.

---
+++ src/Client/wwwroot/scripts/WowheadInterop.ts
@@ -0,0 +1,23 @@
+﻿
+interface WowheadInterop {
+    hide(): void;
+}
+
+interface Window {
+    WhInterop: WowheadInterop;
+    WH: any;
+}
+
+(() => {
+    window.WhInterop = {
+        hide: function () {
+            if (window.WH) {
+                if (window.WH.Tooltip && window.WH.Tooltip.hide) {
+                    window.WH.Tooltip.hide();
+                } else if (window.WH.Tooltips && window.WH.Tooltips.clearTouchTooltip) {
+                    window.WH.Tooltips.clearTouchTooltip();
+                }
+            }
+        }
+    };
+})();
